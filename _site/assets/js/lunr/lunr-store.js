a8ee1bfc37f285c1934ee335ec22e52bba0f4eec
rdgao <r.dg.gao@gmail.com>
2018-02-18 01:06:00 -0800

    changed some config settings, added a test post and included google analytics

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Test Post",
+        "excerpt":"testing testingbold testing","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/test-post/",
+        "teaser":"http://localhost:4000/assets/images/teaser_placeholder.JPG"}]
