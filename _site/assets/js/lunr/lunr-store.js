3097dffcdadd6380c09503d2f62ca5f01402048e
rdgao <r.dg.gao@gmail.com>
2018-02-18 01:34:24 -0800

    still screwing around

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Test Post",
-        "excerpt":"testing testingbold testing","categories": [],
+        "excerpt":"testing testingbold testingwhat is going on I don’t know markdown et al.","categories": [],
         "tags": [],
         "url": "http://localhost:4000/test-post/",
         "teaser":"http://localhost:4000/assets/images/teaser_placeholder.JPG"}]
