5a8d7d27156a8e3ed472d2d58af35f4844b139a2
rdgao <r.dg.gao@gmail.com>
2018-02-19 23:40:30 -0800

    more screwing around & learning the theme settings. single posts now looking good

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Test Post",
-        "excerpt":"testing testingbold testingwhat is going on I don’t know markdown et al.","categories": [],
-        "tags": [],
+        "title": "First post!",
+        "excerpt":"Hi, my name is Bob.bold testingwhat is going on I don’t know markdown et al. what is going on I don’t know markdown et al. what is going on I don’t know markdown et al. what is going on I don’t know markdown et al. what is going on I...","categories": [],
+        "tags": ["holas"],
         "url": "http://localhost:4000/test-post/",
         "teaser":"http://localhost:4000/assets/images/teaser_placeholder.JPG"}]
